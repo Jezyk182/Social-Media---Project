45f7407de4e7fbd1aca756076a614126d258902b
Jezyk182 <plekus73@gmail.com>
2024-05-12 13:46:55 +0200

    initial commit

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="sad-container sad-m-auto">
+      <h1 className="sad-text-3xl sad-font-bold sad-underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
